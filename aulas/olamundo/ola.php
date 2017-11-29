a5342ce9d07852837f476061687c372dd3c86423
Ruan Silva <ruanluiz.as@gmail.com>
2017-11-29 10:08:10 -0200

    aula03

---
+++ aulas/olamundo/ola.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Primeiro Exemplo PHP</title>
+        <style>
+            h2{
+                color: blue;
+                text-shadow: 1px 1px 1px black;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Testando php</h1>
+        <?php
+            echo "<h2>Olá </br> mundo!</h2>";
+        ?>
+    </body>
+</html>
