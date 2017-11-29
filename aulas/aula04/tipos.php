ab3914ab046f790a54645142c490de74d521e259
Ruan Silva <ruanluiz.as@gmail.com>
2017-11-29 10:45:01 -0200

    Aula04

---
+++ aulas/aula04/tipos.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="css/estilo.css"/>
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <?php
+            $n = 4;
+            $nome = (string) "Gustavo";
+            $n = 4.5;
+
+            $idade = 36;
+
+            echo $nome. " tem ".$idade." anos!";
+            echo "</br>";
+            $nome2 = "Maria";
+            $idade2 = 18;
+            echo "$nome2  tem $idade2 anos!";
+            ?>
+        </div>
+    </body>
+</html>
