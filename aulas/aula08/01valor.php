d794dde1bc4e619542e1f60cd5d29d9f7cf11584
Ruan Silva <ruanluiz.as@gmail.com>
2017-12-01 13:59:51 -0200

    aula08 andamento

---
+++ aulas/aula08/01valor.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $valor = $_GET['v'];
+        echo "O valor enviado foi $valor";
+    ?>
+</div>
+</body>
+</html>
+ 
